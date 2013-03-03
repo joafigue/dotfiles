5ca4167606fbc1241cbf023d5d799444e2c0c683
joafigue <joafigue@gmail.com>
2013-03-03 00:13:00 -0300

    Added generic functions to shell file
    which_shell -> Figures out current_shell (generic between bash and ZSH)
    rload       -> Load configuration file

--- shell/Shell_common/shell_functions.sh
+++ shell/Shell_common/shell_functions.sh
@@ -1,1 +1,15 @@
-    
+function which_shell() {
+    local my-shell=""
+    if [ $BASH_VERSION ]; then
+	my_shell=bash
+    elif [ $ZSH_VERSION ]; then
+	my_shell=zsh
+    fi
+    echo $myshell
+}
+
+function rload() {
+local shell=$(which_shell)
+echo $HOME/.${shell}rc
+source  $HOME/.${shell}rc
+}
