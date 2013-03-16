18e594cd9c3560974a5a40157febebf72b0cedf7
joafigue <joafigue@gmail.com>
2013-03-16 19:00:27 -0300

    Modified common shell file to use the functions file. Fixed a bug in the functions file Moved the functions in the common file to the functions file Minor cleanup in common file. Cleaned Whitespace in functions file

--- shell/Shell_common/posix.sh
+++ shell/Shell_common/posix.sh
@@ -1,4 +1,4 @@
-# -*-Bash-script-*- 
+#! /bin/bash
 ################################################################################
 #
 # File:         posix.sh
@@ -18,51 +18,27 @@
 [[ $- != *i* ]] && return
 
 alias ls='ls --color=auto'
-PS1='[\u@\h \W]\$ '
+
+SHELL_SOURCES=$(echo $(ls $HOME -al | grep -m 1 posix.sh ) | sed 's:.*->.\(.*\)\(/Shell_common/posix.sh\).*:\1:')
+SHELL_COMMON=$SHELL_SOURCES/Shell_common
+
+## Source configuration files by functionality
+source $SHELL_COMMON/shell_functions.sh
+source $SHELL_COMMON/emacs_functions.sh
+source $SHELL_COMMON/alias.sh
+
+MY_SHELL=$(which_shell)
+SPECIFIC_SOURCES=$SHELL_SOURCES/$MY_SHELL
+source $SPECIFIC_SOURCES/${MY_SHELL}.${MY_SHELL}
+
+## Clear variables
+unset SHELL_SOURCES
+unset SHELL_COMMON
+unset SPECIFIC_SOURCES
+
 
 EDITOR="emacsclient -t"
 VISUAL="emacsclient -c"
-
-
-
-function colors(){
-#/bin/bash
-#
-#   This file echoes a bunch of color codes to the
-#   terminal to demonstrate what's available.  Each
-#   line is the color code of one forground color,
-#   out of 17 (default + 16 escapes), followed by a
-#   test use of that color on all nine background
-#   colors (default + 8 escapes).
-
-
-T='gYw'   # The test text
-
-echo -e "\n                 40m     41m     42m     43m\
-     44m     45m     46m     47m";
-
-for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
-               '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
-               '  36m' '1;36m' '  37m' '1;37m';
-  do FG=${FGs// /}
-      echo -en " $FGs \033[$FG  $T  "
-        for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
-            do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m";
-              done
-          echo;
-          done
-echo
-}
-
-function clear_whitespace() {
-find . -name '* *' | while read file;
-do
-target=`echo "$file" | sed 's/ /_/g'`;
-echo "Renaming '$file' to '$target'";
-mv "$file" "$target";
-done;
-
-}
 
 
 alias edaemon="emacs --daemon"
--- shell/Shell_common/shell_functions.sh
+++ shell/Shell_common/shell_functions.sh
@@ -1,17 +1,70 @@
 #!/bin/bash
 
 function which_shell() {
-    local my-shell=""
+    local my_shell=""
     if [ $BASH_VERSION ]; then
-	my_shell=bash
+        my_shell=bash
     elif [ $ZSH_VERSION ]; then
-	my_shell=zsh
+        my_shell=zsh
     fi
-    echo $myshell
+    echo $my_shell
 }
 
 function rload() {
     local shell=$(which_shell)
     echo $HOME/.${shell}rc
     source  $HOME/.${shell}rc
+}
+
+function walls() {
+current=$PWD;
+wallpapers=$HOME/Ocio/Pictures/Wallpapers;
+cd $wallpapers
+
+while true; do
+    find ./ -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
+        shuf -n1 -z | xargs -0 feh --bg-fill
+        sleep 40
+done
+
+}
+
+
+function colors(){
+#/bin/bash
+#
+#   This file echoes a bunch of color codes to the
+#   terminal to demonstrate what's available.  Each
+#   line is the color code of one forground color,
+#   out of 17 (default + 16 escapes), followed by a
+#   test use of that color on all nine background
+#   colors (default + 8 escapes).
+
+
+T='gYw'   # The test text
+
+echo -e "\n                 40m     41m     42m     43m\
+     44m     45m     46m     47m";
+
+for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
+               '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
+               '  36m' '1;36m' '  37m' '1;37m';
+  do FG=${FGs// /}
+      echo -en " $FGs \033[$FG  $T  "
+        for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
+            do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m";
+              done
+          echo;
+          done
+echo
+}
+
+function clear_whitespace() {
+find . -name '* *' | while read file;
+do
+target=`echo "$file" | sed 's/ /_/g'`;
+echo "Renaming '$file' to '$target'";
+mv "$file" "$target";
+done;
+
 }
