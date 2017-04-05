c289aaa3d404c89f24af4fd56b976dcb3e32c922
joafigue <joafigue@gmail.com>
2017-04-05 13:02:37 -0300

    Adding slock and improved tmux keybindings
    As it was long overdue I have installed a lock-screen program (slock)
    whose default config is good enough.
    (To log in I only have to type the password)
    
    I also added keybindings for tmux for better navigation to different
    panels (ctrl+arrow) which does not interfere with any emacs key I'm
    currently using

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -8,21 +8,23 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.Tabbed
 import XMonad.Layout.ResizableTile
 import XMonad.Util.EZConfig
+import XMonad.Hooks.EwmhDesktops
 
 
 base_layout = noBorders (Full ||| simpleTabbed )
 tall_layout = ResizableTall 1 (3/100) (1/2) []
 my_layout = base_layout ||| tall_layout
 
-main = xmonad $ defaultConfig
+main = xmonad $ ewmh defaultConfig
     { borderWidth        = 5
     , terminal           = "urxvt"
     , layoutHook         = my_layout
+    , handleEventHook    = handleEventHook defaultConfig <+> fullscreenEventHook 
     , modMask            = mod4Mask
     , normalBorderColor  = "#222222"
-    , focusedBorderColor = "#5555ff"
-    }
+    , focusedBorderColor = "#5555ff" }
     `additionalKeysP`
     [ ("M-S-h", sendMessage MirrorShrink),
-      ("M-S-l", sendMessage MirrorExpand)
+      ("M-S-l", sendMessage MirrorExpand),
+      ("M-C-l", spawn "slock")
     ]
